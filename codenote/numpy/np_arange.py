7cde2e83ac6908ee9532dfe7f759cfa7d7d16fa8
de-yu <symbian3meego@Gmail.com>
2018-05-06 23:24:34 +0800

    codenote

---
+++ codenote/numpy/np_arange.py
@@ -0,0 +1,5 @@
+import numpy as np
+
+number = np.arange(0,10)
+
+print(number)
---
+++ codenote/numpy/np_random_choice.py
@@ -0,0 +1,7 @@
+import numpy as np
+
+batch = np.random.choice(10000, 100)
+
+print(batch)
+
+#  numpy.random.choice(a, size=None, replace=True, p=None)  從 a 中隨機挑選 size 個數字
