423e6a0a19165a88ff1449f47b157d8f6836450c
symbian3 <symbian3@D-IT-DEYUWEI.corp.ruten.com.tw>
2018-05-24 18:09:19 +0800

    del

--- codenote/numpy/np_random_choice.py
+++
@@ -1,7 +0,0 @@
-import numpy as np
-
-batch = np.random.choice(100, 10)
-
-print(batch)
-
-#  numpy.random.choice(a, size=None, replace=True, p=None)  從 a 中隨機挑選 size 個數字
--- codenote/numpy/np_random_randn.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-
-number = np.random.randn(5, 10)#np.random.randn 返回一個 5*10 裡面為常態分布的隨機亂數  回傳陣列
-num_nor = np.random.normal(size = 100000)#num_nor 生成 100000 組標準常態分配（平均值為 0，標準差為 1 的常態分配）隨機變數
-num_uni = np.random.uniform(size = 100000) # 生成 100000 組介於 0 與 1 之間均勻分配隨機變數
-
-print(number)
-
-
